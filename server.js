331c518fff0718c1b4541fb4a0ed025e90b726f5
Nikita Cherevkov <nikita.cherevkov@gmail.com>
2015-03-16 16:36:46 -0400

    implemented serving static files and compiling *.styl into *.css

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+
+function compile(str, path) {
+	return stylus(str).set('filename', path);
+}
+
+
+//express properties
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+
+
+
+//middleware
+app.use(stylus.middleware({
+	src: __dirname + '/public',
+	compile: compile
+}));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
 	res.render('index');
@@ -13,3 +32,4 @@ app.get('*', function(req, res) {
 var port = 3030;
 app.listen(port);
 console.log('Listening on port ' + port + "...");
+
